469e68f06fb83685b0d33a745ad3588e18df965e
user8596904236 <1254955844122064-user8596904236@noreply.coze.cn>
2026-08-09 23:17:19 +0800

    feat(brand): use user PNG as website favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
   description:
     "SublimApparel is a sublimation printing factory in Yiwu, China (est. 2018). Custom print on apparel, mugs, mousepads, banners, home textiles and more. Polyester & 100% cotton. DDP shipping worldwide, US warehouse in Fontana, CA.",
   icons: {
-    icon: "/favicon-temp.jpg",
-    shortcut: "/favicon-temp.jpg",
-    apple: "/favicon-temp.jpg",
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 };
 
